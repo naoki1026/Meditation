6066df45c2c399163ad6cc9b96ffacd7ef2e0add
arakawan <arakawan@pivot.jp>
2020-05-12 21:51:21 +0900

    61まで

--- app/src/main/java/com/example/meditation/MainFragment.kt
+++ app/src/main/java/com/example/meditation/MainFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+//    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +25,36 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.initParameters()
+        btnPlay.setBackgroundResource(R.drawable.ic_play_circle_outline_black_24dp)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.txtLevel.observe(viewLifecycleOwner, Observer { levelTxt ->
+            txtLevel.text = levelTxt
+        })
+        viewModel.txtTheme.observe(viewLifecycleOwner, Observer { themeTxt ->
+            txtTheme.text = themeTxt
+        })
+        viewModel.displayTimeSeconds.observe(viewLifecycleOwner, Observer { displayTime ->
+            txtTime.text = displayTime
+        })
+        viewModel.msgUpperSmall.observe(viewLifecycleOwner, Observer { upperTxt ->
+            txtMsgUpperSmall.text = upperTxt
+        })
+        viewModel.msgLowerLarge.observe(viewLifecycleOwner, Observer { lowerTxt ->
+            txtMsgLowerLarge.text = lowerTxt
+        })
+        viewModel.playStatus.observe(viewLifecycleOwner, Observer {status ->
+            when(status) {
+                PlayStatus.BEFORE_START -> btnPlay.setBackgroundResource(R.drawable.button_play)
+            }
+        })
+    }
+
 
 }
