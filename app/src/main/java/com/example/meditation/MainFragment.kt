7ba571bf21635a69496ced12869389f9c3c16b45
arakawan <arakawan@pivot.jp>
2020-05-12 23:25:39 +0900

    セクション6まで

--- app/src/main/java/com/example/meditation/MainFragment.kt
+++ app/src/main/java/com/example/meditation/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.example.meditation
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import com.example.meditation.databinding.FragmentMainBinding
 import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
@@ -16,45 +23,51 @@ import kotlinx.android.synthetic.main.fragment_main.*
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
-//    private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var binding : FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        val view = binding.root
+        return view
+//        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel.initParameters()
+        binding.apply {
+            viewmodel = viewModel
+            setLifecycleOwner(activity)
+        }
+
         btnPlay.setBackgroundResource(R.drawable.ic_play_circle_outline_black_24dp)
         observeViewModel()
     }
 
     private fun observeViewModel() {
-        viewModel.txtLevel.observe(viewLifecycleOwner, Observer { levelTxt ->
-            txtLevel.text = levelTxt
-        })
-        viewModel.txtTheme.observe(viewLifecycleOwner, Observer { themeTxt ->
-            txtTheme.text = themeTxt
-        })
-        viewModel.displayTimeSeconds.observe(viewLifecycleOwner, Observer { displayTime ->
-            txtTime.text = displayTime
-        })
-        viewModel.msgUpperSmall.observe(viewLifecycleOwner, Observer { upperTxt ->
-            txtMsgUpperSmall.text = upperTxt
-        })
-        viewModel.msgLowerLarge.observe(viewLifecycleOwner, Observer { lowerTxt ->
-            txtMsgLowerLarge.text = lowerTxt
-        })
         viewModel.playStatus.observe(viewLifecycleOwner, Observer {status ->
             when(status) {
                 PlayStatus.BEFORE_START -> btnPlay.setBackgroundResource(R.drawable.button_play)
             }
         })
+        viewModel.themePicFilResId.observe(viewLifecycleOwner, Observer {themePicResId ->
+            loadBackgroundImage(this, themePicResId, meditation_screen)
+        })
     }
 
-
+    private fun loadBackgroundImage(
+        mainFragment: MainFragment,
+        themePicResId: Int?,
+        meditationScreen: ConstraintLayout?
+    ) {
+        Glide.with(mainFragment).load(themePicResId).into(object : SimpleTarget<Drawable>(){
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                meditation_screen?.background = resource
+            }
+        })
+    }
 }
